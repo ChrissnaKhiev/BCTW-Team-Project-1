3dd5a641d1b8d0373072e44bf320e6e563a94221
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-28 10:26:25 -0400

    added movie database alternative api to js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,16 @@
-// 
+
+
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ca455037bmshf2d2d8bbbabf617p1190ecjsn3dd42c18b7d6',
+		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+	}
+};
+
+fetch('https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
