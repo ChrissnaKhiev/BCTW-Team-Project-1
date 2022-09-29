8f791d25b5b32c7185645dd297c108bf5ba783c3
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-29 11:08:32 -0400

    added results population

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,6 +7,7 @@ var genreData = document.getElementById('genre');
 var releasedDate = document.getElementById('released');
 var rated = document.getElementById('rated');
 var rating = document.getElementById('rating');
+var resultsData = document.getElementById('movieResults');
 
 
 // const options = {
@@ -23,7 +24,20 @@ var rating = document.getElementById('rating');
 //         'X-RapidAPI-Host': 'watchmode.p.rapidapi.com'
 //     }
 // };
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '1ca455037bmshf2d2d8bbbabf617p1190ecjsn3dd42c18b7d6',
+// 		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+// 	}
+// };
 
+fetch('./assets/dataS.json')
+	.then(response => response.json())
+	.then(function (data) {
+        getResults(data);
+    })
+	.catch(err => console.error(err));
 function init() {
     fetch('./assets/data.json')
 	.then(response => response.json())
@@ -44,12 +58,13 @@ function init() {
     .catch(err => console.error(err));
 }
 
-function generateSearch() {
-    var homeSearch = document.getElementById('homeInput').value;
-    title = homeSearch;
-    console.log(title);
-    init();
-}
+// function generateSearch() {
+//     var homeSearch = document.getElementById('homeInput').value;
+//     title = homeSearch;
+//     console.log(title);
+//     init();
+// }
+
 function getPoster(data) {
     posterData.innerHTML = `<img src="${data.Poster}">`;
 }
@@ -71,6 +86,12 @@ function getRating(data) {
 }
 function getRate(data) {
     rated.textContent = data.Rated;
+}
+function getResults(data) {
+    resultsData.innerHTML = data.Search.map((Search, index) => {
+        if (index < 13)
+            return `<div class="col"><img src="${Search.Poster}"/><p>${Search.Title}</p></div>`;
+    }).join('');
 }
 
 // homeSearchBtn.addEventListener('click', generateSearch());
