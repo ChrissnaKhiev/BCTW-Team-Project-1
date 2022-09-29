b67d7b6af8121d8de0837d3a6dc1d9a90c28e00b
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-28 22:46:25 -0400

    added sample api data for both apis

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,41 @@
-var wlist
+var wlist;
+var homeSearchBtn = document.getElementById('homeSearchBtn');
+var title = '';
 
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '1ca455037bmshf2d2d8bbbabf617p1190ecjsn3dd42c18b7d6',
-        'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-    }
-};
-const options1 = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '1ca455037bmshf2d2d8bbbabf617p1190ecjsn3dd42c18b7d6',
-        'X-RapidAPI-Host': 'watchmode.p.rapidapi.com'
-    }
-};
 
-fetch('https://movie-database-alternative.p.rapidapi.com/?r=json&i=tt4154796', options)
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '1ca455037bmshf2d2d8bbbabf617p1190ecjsn3dd42c18b7d6',
+//         'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+//     }
+// };
+// const options1 = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '1ca455037bmshf2d2d8bbbabf617p1190ecjsn3dd42c18b7d6',
+//         'X-RapidAPI-Host': 'watchmode.p.rapidapi.com'
+//     }
+// };
+
+function init() {
+    fetch('./assets/data.json')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+    fetch('./assets/sample.json')
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
-fetch('https://watchmode.p.rapidapi.com/title/3173903/details/', options1)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+}
+
+function generateSearch() {
+    var homeSearch = document.getElementById('homeInput').value;
+    title = homeSearch;
+    console.log(title);
+    init();
+}
+
+homeSearchBtn.addEventListener('click', generateSearch());
+init();
 
