009817ab3f5d4aeff51d665edb7493616ed807ef
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-30 10:44:03 -0400

    fixed js function for movie.html

--- Develop/script/script.js
+++ Develop/script/script.js
@@ -44,11 +44,12 @@ fetch('./assets/dataS.json')
 	.then(function (data) {
         getResults(data);
     })
-	.catch(err => console.error(err));
+	// .catch(err => console.error(err));
 function init() {
     fetch('./assets/data.json')
 	.then(response => response.json())
-	.then( function (data) {
+	.then(function (data) {
+        console.log(data);
         getPoster(data);
         getPlot(data);
         getTitle(data);
@@ -94,12 +95,6 @@ function getResults(data) {
             return `<div class="col"><img src="${Search.Poster}"/><p>${Search.Title}</p></div>`;
     }).join('');
 }
-
-function myClick() {
-        window.location.replace("results.html")
-}
-homeSearchBtn.addEventListener('click', myClick);
-
 function getLocations(data) {
     console.log(data);
     listLocation.innerHTML = data.map((newData) => {
@@ -113,7 +108,9 @@ function addMovie() {
     console.log("movie poster", posterData.innerHTML);
 }
 
-addToWatchList.addEventListener("click", addMovie)
+addToWatchList.addEventListener("click", addMovie);
+
+init();
 
 // RETURN TO INDEX
 var homeBtn = document.getElementsByClassName('logo');
@@ -121,3 +118,8 @@ homeBtn[0].addEventListener("click", homeBound);
 function homeBound(){
 window.location.replace("./index.html");}
 // 
+
+function myClick() {
+    window.location.replace("./results.html");
+}
+homeSearchBtn.addEventListener('click', myClick);
